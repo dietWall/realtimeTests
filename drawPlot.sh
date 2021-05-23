9e9f6c38d7b4f6924b45501f10ae745ba50ee7f9
Dietrich Wall <dietrich.wall@emocean.io>
2021-05-23 18:05:53 +0200

    divided sched_latency.sh in 2 scripts: 1 for creating output 1 for drawing the plot

---
+++ drawPlot.sh
@@ -0,0 +1,57 @@
+cyclictestoutputfile=test_output
+
+while getopts f flag
+do
+	case "${flag}" in
+		f) cyclictestoutputfile=${OPTARG};;
+	esac
+done
+
+#Get maximum latency
+max=`grep "Max Latencies" ${cyclictestoutputfile} | tr " " "\n" | sort -n | tail -1 | sed s/^0*//`
+
+#Grep data lines, remove empty lines and create a common field separator
+grep -v -e "^#" -e "^$" ${cyclictestoutputfile} | tr " " "\t" >histogram 
+
+#Set the number of cores, for example
+cores=$(lscpu | grep 'CPU(s):' | awk '{print $2}')
+
+echo "Calculation with Number of Cores: ${cores}"
+
+#Create two-column data sets with latency classes and frequency values for each core, for example
+for i in `seq 1 $cores`
+do
+  column=`expr $i + 1`
+  cut -f1,$column histogram >histogram$i
+done
+
+#Create plot command header
+echo -n -e "set title \"Latency plot\"\n\
+set terminal png\n\
+set xlabel \"Latency (us), max $max us\"\n\
+set logscale y\n\
+set xrange [0:400]\n\
+set yrange [0.8:*]\n\
+set ylabel \"Number of latency samples\"\n\
+set output \"plot.png\"\n\
+plot " >plotcmd
+
+#Append plot command data references
+for i in `seq 1 $cores`
+do
+  if test $i != 1
+  then
+    echo -n ", " >>plotcmd
+  fi
+  cpuno=`expr $i - 1`
+  if test $cpuno -lt 10
+  then
+    title=" CPU$cpuno"
+   else
+    title="CPU$cpuno"
+  fi
+  echo -n "\"histogram$i\" using 1:2 title \"$title\" with histeps" >>plotcmd
+done
+
+#Execute plot command
+gnuplot -persist <plotcmd
