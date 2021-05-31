a5efff35939d1e56efae3d725762af946993815e
DietrichWall <dietrich.wall@emocean.io>
2021-05-31 16:04:25 +0200

    updated xticks font size

--- drawPlot.sh
+++ drawPlot.sh
@@ -32,6 +32,7 @@ set terminal png\n\
 set xlabel \"Latency (ns), max $max ns\"\n\
 set logscale y\n\
 set xrange [1000:20000]\n\
+set xtics font \" ,6\"\n\
 set yrange [0.8:*]\n\
 set ylabel \"Number of latency samples\"\n\
 set output \"plot.png\"\n\
